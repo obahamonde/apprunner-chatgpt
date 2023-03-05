ef4018880377ea12ff95ea2e58f7265cfcaedc9c
Oscar Bahamonde <107950590+obahamonde@users.noreply.github.com>
2023-03-04 20:09:31 -0500

    f

--- src/config.py
+++ src/config.py
@@ -1,11 +1,16 @@
+from dotenv import load_dotenv
 from pydantic import BaseSettings, BaseConfig, Field
+import os
+
+load_dotenv()
+
 
 class Settings(BaseSettings):
     """Settings for the application
     """
-    API_KEY: str = Field(..., env="API_KEY")
-    FAUNA_SECRET: str = Field(..., env="FAUNA_SECRET")
-    AUTH0_DOMAIN: str = Field(..., env="AUTH0_DOMAIN")
+    API_KEY: str = Field(default = os.environ.get("API_KEY"))   
+    FAUNA_SECRET: str = Field(default=os.environ.get("FAUNA_SECRET"))
+    AUTH0_DOMAIN: str = Field(default=os.environ.get("AUTH0_DOMAIN"))
     
     class Config(BaseConfig):
         env_file = ".env"
