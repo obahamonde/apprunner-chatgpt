c7db14acdd9c83a190ff8a8faf7f752499fc2c81
Oscar Bahamonde <107950590+obahamonde@users.noreply.github.com>
2023-03-23 13:21:19 -0500

    baackend only

--- main.py
+++ main.py
@@ -50,4 +50,4 @@ async def startup():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=3000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=3000)
